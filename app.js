1e2ab4c207270f3348fe721b8089a1a484649fe4
kevenruiz <kevenruizh@gmail.com>
2021-03-25 16:48:25 -0700

    entered app.js info

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // import functions and grab DOM elements
-
+const button =document.getElementById('guess-button');
+const whoWonEl = document.getElementById('who-won');
+const
 // initialize state
 
 // set event listeners to update state and DOM
