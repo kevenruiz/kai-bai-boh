815e4ed62d71dd0cb418d684b19a625b9411cd38
keven ruiz <kevenruizh@gmail.com>
2021-03-26 05:21:37 -0700

    added more utils code

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // import functions and grab DOM elements
+import { getActualGuess } from '../utils.js';
+
 const buttonEl = document.getElementById('guess-button');
 const whoWonEl = document.getElementById('who-won');
 const winsEl = document.getElementById('wins');
@@ -10,13 +12,16 @@ let total = 0;
 
 // set event listeners to update state and DOM
 
-buttonEl.addEventListener('click'), () => {
+buttonEl.addEventListener('click', () => {
 
-    // 1) User clicks submit
-    //   -randomly pick a computer throw ('rock' | 'paper' | 'Scissors')
-    const randomNumber = Math.floor();
+});
 
-    const actualGuess = getActualGuess(randomNumber);
+// 1) User clicks submit
+//   -randomly pick a computer throw ('rock' | 'paper' | 'Scissors')
+const randomNumber = Math.floor();
+//
+const actualGuess = getActualGuess(randomNumber);
+console.log(getActualGuess);
     //   -get the user's guess
     //       -It lives in the radio button somehow
     //   -compare user guess to computer throw
@@ -25,4 +30,3 @@ buttonEl.addEventListener('click'), () => {
     //   -increment the win, if the use got it right. 
     //   -display new values for total, wins, and losses. 
 
-};
